b73d622b51f61239a220e940f4ead6cedebee03b
ANonymous <postmaster.richmama@gmail.com>
2018-05-02 23:38:58 +0100

    overhaul

--- resources/views/emails/bomb.blade.php
+++ resources/views/emails/bomb.blade.php
@@ -1,7 +1,6 @@
-
-Hello,<br>
+<p>Hello,</p><br>
 <p>I sent you a mail last week. I'm still waiting for your reply.</p><br>
-Thanks<br>
-Chris Woods<br>
+<p>Thanks</p><br>
+<p>Chris Woods</p><br>
 
-<small style="font-color: #dedede;">Sent from my T-Mobile 4G LTE device</small><br>
+<small style="font-color: #dedede;"><em>Sent from my T-Mobile 4G LTE device</em></small><br>
